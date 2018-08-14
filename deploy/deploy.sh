da04fe65ee772c5966f13f6489a992dd18573495
Andrew Klinker <aklinker@sourceallies.com>
2018-08-14 11:21:07 -0500

    SFT-103 Remove echo and add custom headers

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -14,7 +14,6 @@ aws s3api wait bucket-not-exists --bucket $bucketName
 echo "Creating a new S3 bucket..."
 aws s3 mb s3://$bucketName
 aws s3api wait bucket-exists --bucket $bucketName
-echo $bucketName
 
 echo "Putting the zipped code into the S3 bucket..."
 aws s3api put-object --bucket $bucketName --key artifact.zip --body ../artifact.zip
--- deploy/local/deploy.sh
+++ deploy/local/deploy.sh
@@ -13,7 +13,6 @@ aws s3api wait bucket-not-exists --bucket $bucketName
 echo "Creating a new S3 bucket..."
 aws s3 mb s3://$bucketName
 aws s3api wait bucket-exists --bucket $bucketName
-echo $bucketName
 
 echo "Putting the zipped code into the S3 bucket..."
 aws s3api put-object --bucket $bucketName --key artifact.zip --body ../../artifact.zip
@@ -34,7 +33,7 @@ aws cloudformation deploy --stack-name $stackName \
         ClientSecret=$CLIENT_SECRET \
         BucketName=$bucketName \
         ApiUrl=$API_URL \
-        OAuthCustomHeaders="application/x-www-form-urlencoded" \
+        OAuthCustomHeaders=$OAUTH_CUSTOM_HEADERS \
         AuthorizeCallbackUri=$AUTHORIZE_CALLBACK_URI \
     --no-fail-on-empty-changeset \
 
